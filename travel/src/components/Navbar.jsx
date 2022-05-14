163d09c4f59d06a3f98b9acee7e5a7f16a68a041
Vailery <leraliabik8@gmail.com>
2022-05-14 13:31:57 +0300

    feat: add a hero section

--- travel/src/components/Navbar.jsx
+++ travel/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between items-center h-20 px-4">
+    <div className="flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white">
       <div>
         <h1 onClick={handleNav} className={logo ? "hidden" : "block"}>
           BEACHES.
@@ -43,16 +43,16 @@ const Navbar = () => {
 
       <div onClick={handleNav} className=" md:hidden z-10">
         {nav ? (
-          <AiOutlineClose className="text-black" size={20} />
+          <AiOutlineClose className="text-black cursor-pointer" size={20} />
         ) : (
-          <HiOutlineMenuAlt4 size={20} />
+          <HiOutlineMenuAlt4 size={20} className="cursor-pointer" />
         )}
       </div>
       <div
         onClick={handleNav}
         className={
           nav
-            ? "absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col"
+            ? "absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col"
             : "absolute left-[-100%]"
         }
       >
