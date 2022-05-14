5b73d7be6a6f15242a3e3afe8d8cfc7505fa1e6b
Vailery <leraliabik8@gmail.com>
2022-05-14 19:17:22 +0300

    refactor: change carousel

--- travel/src/components/Carousel.jsx
+++ travel/src/components/Carousel.jsx
@@ -1,48 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { BsArrowLeftSquareFill, BsArrowRightSquareFill } from "react-icons/bs";
 
-const sliderData = [
-  {
-    url: "https://images.unsplash.com/photo-1515238152791-8216bfdf89a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-  },
-];
+import slide1 from "../assets/slide1.jpg";
+import slide2 from "../assets/slide2.jpg";
+import slide3 from "../assets/slide3.jpg";
 
-const Carousel = () => {
-  const [slide, setSlide] = useState(0);
-  const length = sliderData.length;
+export default function Carousel() {
+  const [idx, setIdx] = useState(0);
+  const sliderData = [slide1, slide2, slide3];
+  let count = 0;
 
-  const prevSlide = () => {
-    setSlide(slide === length - 1 ? 0 : slide + 1);
+  const slideRef = useRef();
+
+  const handleOnNextClick = () => {
+    const productsLength = sliderData.length;
+    count = (idx + productsLength + 1) % productsLength;
+    setIdx(count);
+    slideRef.current.classList.add("fade-anim");
   };
-  const nextSlide = () => {
-    setSlide(slide === 0 ? length - 1 : slide - 1);
+
+  const handleOnPrevClick = () => {
+    const productsLength = sliderData.length;
+    count = (idx + productsLength - 1) % productsLength;
+    setIdx(count);
+    slideRef.current.classList.add("fade-anim");
   };
 
   return (
-    <div className="max-w-[1240px] mx-auto px-4 py-16 relative flex justify-center itmes-center">
+    <div
+      ref={slideRef}
+      className="max-w-[1240px] mx-auto px-4 py-16 relative flex justify-center items-center"
+    >
       <BsArrowLeftSquareFill
-        onClick={prevSlide}
         className="absolute top-[50%] text-3xl text-white cursor-pointer left-8"
+        onClick={handleOnPrevClick}
       />
+
       <BsArrowRightSquareFill
-        onClick={nextSlide}
         className="absolute top-[50%] text-3xl text-white cursor-pointer right-8"
+        onClick={handleOnNextClick}
       />
-      {sliderData.map((item, index) => (
-        <div className={index === slide ? "opacity-100" : "opacity-0"}>
-          {index === slide && (
-            <img className="w-full rounded-md" src={item.url} alt="/" />
-          )}
-        </div>
-      ))}
+
+      <img className="w-full rounded-md" src={sliderData[idx]} alt="/" />
     </div>
   );
-};
-
-export default Carousel;
+}
